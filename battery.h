854608ad59300a74ddd0f654f53e1db3251b3c5d
nur <nur.php@gmail.com>
2020-07-29 05:43:45 +0800

    initial;

---
+++ battery.h
@@ -0,0 +1,6 @@
+#ifndef _BATTERY_H
+#define _BATTERY_H
+extern uint16 getBatteryVoltage(void);
+extern uint8 getBatteryVoltageZCL(void);
+extern uint8 getBatteryRemainingPercentageZCL(void);
+#endif
