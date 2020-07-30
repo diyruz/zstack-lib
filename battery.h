85121df699063375b4557247f4507986be5dc023
nur <nur.php@gmail.com>
2020-07-31 05:49:14 +0800

    fix battery measurements

--- battery.h
+++ battery.h
@@ -1,6 +1,6 @@
 #ifndef _BATTERY_H
 #define _BATTERY_H
 extern uint16 getBatteryVoltage(void);
-extern uint8 getBatteryVoltageZCL(void);
-extern uint8 getBatteryRemainingPercentageZCL(void);
+extern uint8 getBatteryVoltageZCL(uint16 millivolts);
+extern uint8 getBatteryRemainingPercentageZCL(uint16 millivolts);
 #endif
--- bettery.c
+++ bettery.c
@@ -2,28 +2,28 @@
 #include "battery.h"
 #include "hal_adc.h"
 #include "utils.h"
-// (( 3 * 1.15 ) / (( 2^14 / 2 ) - 1 )) * 1000
-#define MULTI (double) 0.4211939934
+// (( 3 * 1.15 ) / (( 2^14 / 2 ) - 1 )) * 1000 (not correct)
+// #define MULTI (float) 0.4211939934
+// this coefficient calculated using
+// https://docs.google.com/spreadsheets/d/1qrFdMTo0ZrqtlGUoafeB3hplhU3GzDnVWuUK4M9OgNo/edit?usp=sharing
+#define MULTI (float)0.443
 
-#define VOLTAGE_MIN (double) 2.0
-#define VOLTAGE_MAX (double) 3.3
+#define VOLTAGE_MIN (double)2.0
+#define VOLTAGE_MAX (double)3.3
 
-uint8 getBatteryVoltageZCL(void) {
-    uint16 volt16 = getBatteryVoltage();
-    uint8 volt8 = (uint8)(volt16 / 100);
-    if ((volt16 - (volt8 * 100)) > 50) {
+uint8 getBatteryVoltageZCL(uint16 millivolts) {
+    uint8 volt8 = (uint8)(millivolts / 100);
+    if ((millivolts - (volt8 * 100)) > 50) {
         return volt8 + 1;
     } else {
         return volt8;
     }
 }
-//return millivolts
+// return millivolts
 uint16 getBatteryVoltage(void) {
     HalAdcSetReference(HAL_ADC_REF_125V);
     double rawADC = adcReadSampled(HAL_ADC_CHANNEL_VDD, HAL_ADC_RESOLUTION_14, HAL_ADC_REF_125V, 10);
-    return (uint16) (rawADC * MULTI);
+    return (uint16)(rawADC * MULTI);
 }
 
-uint8 getBatteryRemainingPercentageZCL(void) {
-    return mapRange(VOLTAGE_MIN, VOLTAGE_MAX, 0, 200, getBatteryVoltage());
-}
+uint8 getBatteryRemainingPercentageZCL(uint16 millivolts) { return mapRange(VOLTAGE_MIN, VOLTAGE_MAX, 0, 200, millivolts); }
